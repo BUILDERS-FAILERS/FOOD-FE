0708bb0763d3d0736e0f9e5a7a8c9fb0a509a1f4
Abdullah <boomjay008@gmail.com>
2023-08-17 11:23:25 +0100

    eslint fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import PageNav from "@/components/PageNav";
+import PageNav from "@/components/PageNav"
 
 export default function Home() {
-  return (
-    <main className="p-3 bg-background">
-      <div className="flex min-h-screen flex-col items-center justify-between md:px-20 md:py-8 bg-theme rounded-3xl">
-        <PageNav />
-      </div>
-    </main>
-  )
+	return (
+		<main className="p-3 bg-background">
+			<div className="flex min-h-screen flex-col items-center justify-between md:px-20 md:py-8 bg-theme rounded-3xl">
+				<PageNav />
+			</div>
+		</main>
+	)
 }
